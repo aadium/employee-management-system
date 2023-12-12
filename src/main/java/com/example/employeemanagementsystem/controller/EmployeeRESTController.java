eff4150b9ff22b2cf47a9112a8bd1f813b92dc96
Aadi Umrani <47914987+aadium@users.noreply.github.com>
2023-12-12 18:25:09 -0500

    Created basic UI for viewing retrived data

--- src/main/java/com/example/employeemanagementsystem/controller/EmployeeRESTController.java
+++ src/main/java/com/example/employeemanagementsystem/controller/EmployeeRESTController.java
@@ -4,19 +4,24 @@ import com.example.employeemanagementsystem.model.Employee;
 import com.example.employeemanagementsystem.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/employees")
+@RequestMapping("/")
 public class EmployeeRESTController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping
-    public List<Employee> getAllEmployees() {
-        return employeeService.getAllEmployees();
+    @GetMapping("/employees")
+    public ModelAndView getAllEmployees() {
+        List<Employee> employees = employeeService.getAllEmployees();
+        System.out.println("Number of employees: " + employees.size());
+        ModelAndView modelAndView = new ModelAndView("employees");
+        modelAndView.addObject("employees", employees);
+        return modelAndView;
     }
 
     @GetMapping("/{id}")
